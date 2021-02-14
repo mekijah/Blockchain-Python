8f31e559bdc3edbb1da002bfc21f3cf36b38f9e7
mekijah <femgeek.massey@gmail.com>
2021-02-14 15:33:30 -0500

    update wallet info

---
+++ wallet/wallet.py
@@ -0,0 +1,14 @@
+import subprocess
+import json
+import pprint
+
+command = '/Users/mischelle.massey/Desktop/Jupyter lab/Blockchain Tools/wallet/hd-wallet-derive/derive -g --mnemonic="order arrange soup change add security mouse emerge else erase puzzle stick" --cols=path,address,privkey,pubkey -g --format=json'
+
+p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()
+p_status = p.wait()
+
+
+keys=json.loads(output)
+print(keys)
+
