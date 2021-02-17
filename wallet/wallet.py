58a7622a21cd2431ef53e1b9386be16a55cd06ad
mekijah <femgeek.massey@gmail.com>
2021-02-17 17:15:10 -0500

    Add updated HW files

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -2,7 +2,7 @@ import subprocess
 import json
 import pprint
 
-command = '/Users/mischelle.massey/Desktop/Jupyter lab/Blockchain Tools/wallet/hd-wallet-derive/derive -g --mnemonic="order arrange soup change add security mouse emerge else erase puzzle stick" --cols=path,address,privkey,pubkey -g --format=json'
+command = './derive -g --mnemonic="order arrange soup change add security mouse emerge else erase puzzle stick" --cols=path,address,privkey,pubkey -g --format=json'
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 (output, err) = p.communicate()
